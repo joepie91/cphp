ede5d9f3fea43ccf09e7d34ff16feaa61ff54f5b
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-05-04 17:20:02 +0200

    Initial commit

---
+++ config.mysql.sample.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * CPHP is more free software. It is licensed under the WTFPL, which
+ * allows you to do pretty much anything with it, without having to
+ * ask permission. Commercial use is allowed, and no attribution is
+ * required. We do politely request that you share your modifications
+ * to benefit other developers, but you are under no enforced
+ * obligation to do so :)
+ * 
+ * Please read the accompanying LICENSE document for the full WTFPL
+ * licensing text.
+ */
+
+$cphp_mysql_host = "localhost";
+$cphp_mysql_user = "root";
+$cphp_mysql_pass = "";
+$cphp_mysql_db 	= "";
+
+?>
